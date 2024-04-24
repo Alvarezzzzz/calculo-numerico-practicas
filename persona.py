db30469167dfae62f751b5de335d4a31bcc1afa4
Alvarez <122706047+Alvarezzzzz@users.noreply.github.com>
2024-04-24 07:01:30 -0400

    Update persona.py

--- persona.py
+++ persona.py
@@ -27,7 +27,7 @@ class Persona:
 
 n = "asaskhjfbksadf"
 while not n.isnumeric():
-    n = input("Coloque el numero de personas: ")
+    n = input("Coloque la cantidad de personas: ")
     if not n.isnumeric():
         print("Valor invalido")
 n = int(n)
